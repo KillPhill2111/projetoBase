a7300b36484e0cea8a025d6e9ef7988d4134dfe9
Filipe Fonseca <filipe.fonseca.eng@outlook.com>
2023-10-23 10:42:55 -0300

    Aula23-10 Intefaces e Validações

---
+++ HelperStockBeta/HelperStockBeta.Domain/Interface/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using HelperStockBeta.Domain.Entities;
+
+namespace HelperStockBeta.Domain.Interface
+{
+    public interface ICategoryRepository
+    {
+        //Assinaturas Customizadas
+        
+        Task<IEnumerable<Category>> GetCategoriesAsync();//a tarefa traz um array 
+        Task<Category> GetIdByAsync(int id);//a tarefa necessita um Id
+
+        //Assinatura de CRUD
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
